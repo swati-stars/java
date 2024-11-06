99dde6818cb5ee774a83ee6c254d4cf28a4854e6
swati jain <jainswati21march@gmail.com>
2024-11-06 20:46:55 +0530

    java Step1

---
+++ src/main/java/codingclub/HelloWorld.java
@@ -0,0 +1,9 @@
+package codingclub;
+import java.util.Date;
+
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("HELLO WORLD! is runnig at " +new Date().toString()+ "sharp.");
+
+    }
+}
